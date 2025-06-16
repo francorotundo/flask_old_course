615c6e318bb6c4b457946e1c7a0cf28fae672dd4
francorotundo90@gmail.com <francisco.rotundo@alsglobal.com>
2025-06-16 15:53:22 -0400

    feat: Control structures

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, make_response, redirect, render_template, request
 
 app = Flask(__name__)
 
+todos = [f'TODO {i}' for i in range(1,5)]
 @app.route('/hello', methods=["GET"])
 def hello_world():
     user_ip = request.cookies.get('user_ip')
-    return render_template('hello.html', user_ip=user_ip)
+    context = {
+        "user_ip":user_ip, 
+        "todos":todos
+    }
+    return render_template('hello.html', **context)
 
 @app.route('/', methods=['GET'])
 def index():
