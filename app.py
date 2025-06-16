e6027674bb737ef81989254a260d7e1b94c1a153
francorotundo90@gmail.com <francisco.rotundo@alsglobal.com>
2025-06-16 15:21:06 -0400

    feat: Flask Basics

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, make_response, redirect, request
+
+app = Flask(__name__)
+
+@app.route('/hello', methods=["GET"])
+def hello_world():
+    user_ip = request.cookies.get('user_ip')
+    return f"Hello {user_ip}"
+
+@app.route('/', methods=['GET'])
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+    
+    return response
+    
